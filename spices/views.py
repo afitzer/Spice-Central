e4d8633fa7e28cd6f981819b0a5642a1af800bbb
afitzer <alexpfitzgerald@yahoo.com>
2023-01-02 09:13:04 -0600

    Updated the templates to pull from the db

--- spices/views.py
+++ spices/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
-    return render(request, 'spices/index.html')
+    products = Product.objects.all()
+    return render(request, 'spices/index.html', {'products': products})
 
 def shop(request):
-    return render(request, 'spices/shop.html')
+    products = Product.objects.all()
+    return render(request, 'spices/shop.html', {'products': products})
 
 def about(request):
     return render(request, 'spices/about.html')
